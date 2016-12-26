e615a235bff8e10693d316b9f40095b4d1a16f73
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-12-26 21:05:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IKIT_taxi;
+
+namespace Tax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Taxi> cars = new List<Taxi>();
+            Taxi car1 = new Taxi("Волга", 4, 15, 10);
+            Taxi car2 = new Taxi("Микроавтобус", 8, 40, 15);
+            Taxi car3 = new Taxi("Ока", 2, 24, 8);
+            cars.Add(car1);
+            cars.Add(car2);
+            cars.Add(car3);
+            Console.Write("Введите расстояние: ");
+            double Distance = double.Parse(Console.ReadLine());
+            Console.Write("Введите количество пассажиров: ");
+            int Pass = int.Parse(Console.ReadLine());
+            foreach (var car in cars)
+            {
+                if (car.InspecOrder(Distance, Pass))
+                {
+                    Console.WriteLine(car.Name);
+                    cars.Remove(car);
+                    cars.Add(car);
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKIT_taxi
+{
+
+    public struct Taxi
+    {
+        public string Name;
+        public int Seat;
+        public double Rest;
+        public double Rate;
+
+        public Taxi(string Name, int Seat, double Rest, double Rate)
+        {
+            this.Name = Name;
+            this.Seat = Seat;
+            this.Rest = Rest;
+            this.Rate = Rate;
+        }
+
+        public bool InspecOrder(double Distance, int Pass)
+        {
+            if (((Rate / 100 * Distance) < Rest) && (Seat >= Pass))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
